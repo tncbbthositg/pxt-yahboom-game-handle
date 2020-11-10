3f103046cc394330cf29b291ecf443fe508edf71
Patrick Caldwell <tncbbthositg@gmail.com>
2020-11-10 17:44:34 -0500

    initial commit of some stubbed out metadata

---
+++ README.md
@@ -0,0 +1,12 @@
+# pxt-yahboom-game-handle
+
+A PXT extension for the Yahboom micro:bit basic game handle.
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+## License
+
+MIT
---
+++ yahboom_game_handle.ts
@@ -0,0 +1,27 @@
+enum YahboomGameHandleButton {
+  //% block=red
+  Red = DigitalPin.P13,
+  //% block=green
+  Green = DigitalPin.P14,
+  //% block=yellow
+  Yellow = DigitalPin.P15,
+  //% block=blue
+  Blue = DigitalPin.P16,
+  //% block=joystick
+  Joystick = DigitalPin.P8,
+}
+
+/**
+ * Controls the functions of the Yahboom Basic Game Handle
+ */
+//% color=#5197D5 weight=100 icon="\uf11b"
+//% groups='["Buttons", "Joystick", "Outputs"]'
+namespace yahboomGameHandle {
+  /**
+   * Returns true if the button is down
+   * @param button the button to test
+   */
+  export function isButtonDown(button: YahboomGameHandleButton): boolean {
+    return pins.digitalReadPin(button) === 1;
+  }
+}
