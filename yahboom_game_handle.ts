8a506b67976519a99ddf30ab23e61e84cb436157
Patrick Caldwell <tncbbthositg@gmail.com>
2020-11-10 17:46:21 -0500

    did not load . . . maybe because I am using DigitalPin?

--- yahboom_game_handle.ts
+++ yahboom_game_handle.ts
@@ -1,14 +1,14 @@
 enum YahboomGameHandleButton {
   //% block=red
-  Red = DigitalPin.P13,
+  Red = "DigitalPin.P13",
   //% block=green
-  Green = DigitalPin.P14,
+  Green = "DigitalPin.P14",
   //% block=yellow
-  Yellow = DigitalPin.P15,
+  Yellow = "DigitalPin.P15",
   //% block=blue
-  Blue = DigitalPin.P16,
+  Blue = "DigitalPin.P16",
   //% block=joystick
-  Joystick = DigitalPin.P8,
+  Joystick = "DigitalPin.P8",
 }
 
 /**
