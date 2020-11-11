dce789427bfc96937bcf55624114b994f5c1d9b2
Patrick Caldwell <tncbbthositg@gmail.com>
2020-11-10 20:31:15 -0500

    initializing for edge events

--- yahboom_game_handle.ts
+++ yahboom_game_handle.ts
@@ -46,11 +46,18 @@ namespace gameController {
 
 function init(): void {
     hasBeenInitialized = true;
+
     pins.setPull(DigitalPin.P13, PinPullMode.PullUp);
     pins.setPull(DigitalPin.P14, PinPullMode.PullUp);
     pins.setPull(DigitalPin.P15, PinPullMode.PullUp);
     pins.setPull(DigitalPin.P16, PinPullMode.PullUp);
     pins.setPull(DigitalPin.P8, PinPullMode.PullUp);
+
+    pins.setEvents(DigitalPin.P13, PinEventType.Edge);
+    pins.setEvents(DigitalPin.P14, PinEventType.Edge);
+    pins.setEvents(DigitalPin.P15, PinEventType.Edge);
+    pins.setEvents(DigitalPin.P16, PinEventType.Edge);
+    pins.setEvents(DigitalPin.P8, PinEventType.Edge);
   }
 
   /**
